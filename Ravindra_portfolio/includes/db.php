08891d8360ade068d696eea47196eb2d6e52a02a
RAVINDRA SINGH <ravindradeeg95@gmail.com>
2026-06-08 15:59:52 +0530

    Update db.php

--- Ravindra_portfolio/includes/db.php
+++ Ravindra_portfolio/includes/db.php
@@ -3,10 +3,10 @@
    Database connection (MySQL via PDO)
    ---> EDIT the four values below to match your phpMyAdmin DB.
    ============================================================ */
-$DB_HOST = 'sql113.infinityfree.com';          // usually 'localhost'
-$DB_NAME = 'if0_42104284_portfolio';          // the database name you created in phpMyAdmin
-$DB_USER = 'if0_42104284';               // your MySQL username
-$DB_PASS = '7568565831';                   // your MySQL password ('' for local XAMPP/WAMP)
+$DB_HOST = '127.0.0.1';
+$DB_NAME = 'portfolio';
+$DB_USER = 'root';      // default local user
+$DB_PASS = '';                   // your MySQL password ('' for local XAMPP/WAMP)
 
 function get_db() {
   global $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS;
